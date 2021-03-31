0e4ae8b35195abe5eb308b5bffc0e67bb37e98b7
Vladimir Stepin <vladimirstyopin@mail.ru>
2021-03-31 12:45:39 +0300

    step_5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+import evenCode from '../src/even-code.js';
 import cli from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = cli();
 console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+evenCode(name);
